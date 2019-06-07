66422b98cb1bd9487748cb21055a440972d4c5c5
housw <housw2010@gmail.com>
2019-06-07 16:43:19 -0700

    remove old BEARS

--- BEARS/bears/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""Top-level package for BEARS."""
-
-__author__ = """Shengwei Hou"""
-__email__ = 'housw2010@gmail.com'
-__version__ = '0.1.0'
--- BEARS/docs/usage.rst
+++
@@ -1,7 +0,0 @@
-=====
-Usage
-=====
-
-To use BEARS in a project::
-
-    import bears
